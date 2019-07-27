819571d54aee0b1c44ba901a2d8d4cb356375901
Varinder Singh <Varinder.Singh@timesinternet.in>
2019-07-27 16:39:10 +0530

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const myfirstelement = '<h1>Hello React!</h1>';
+
+ReactDOM.render(myfirstelement, document.getElementById('root'));
+
+/*
+You are now watching a React file
+through our 'Show React' tool.
+Click the tab to check out
+the 'index.html' file as well.
+*/
